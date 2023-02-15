f316deba9ac1317296d66ec19300805d9b6fac59
junaid ahmed <junaidahmed@junaids-MacBook-Pro.local>
2023-02-15 11:24:47 +0100

    removing unnecessary files

--- src/main/java/com/postnord/kafka/controller/Postnord.java
+++ src/main/java/com/postnord/kafka/controller/Postnord.java
@@ -8,21 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.postnord.kafka.model.User;
 import com.postnord.kafka.service.Producer;
-import com.postnord.kafka.utils.Constants;
 
-import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
 
-import static java.lang.Runtime.getRuntime;
 
 
 @RestController
