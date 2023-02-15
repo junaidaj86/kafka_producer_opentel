2e5839f3ec111d7a43c75797c590fa5fe79786b7
junaid ahmed <junaidahmed@junaids-MacBook-Pro.local>
2023-02-15 11:23:49 +0100

    removing unnecessary files

--- src/main/java/com/postnord/kafka/OpentelApplication.java
+++ src/main/java/com/postnord/kafka/OpentelApplication.java
@@ -1,8 +1,12 @@
 package com.postnord.kafka;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -11,5 +15,16 @@ public class OpentelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OpentelApplication.class, args);
 	}
+
+	@Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(15);
+        executor.setQueueCapacity(9000);
+        executor.setThreadNamePrefix("KafkaMsgExecutor-");
+        executor.initialize();
+        return executor;
+    }
 
 }
--- src/main/java/com/postnord/kafka/controller/Postnord.java
+++ src/main/java/com/postnord/kafka/controller/Postnord.java
@@ -31,11 +31,7 @@ public class Postnord {
     @Autowired
     Producer producer;
 
-    @Value("otel.traces.api.version")
-    private String tracesApiVersion;
-
-    @Value("otel.metrics.api.version")
-    private String metricsApiVersion;
+  
 
     @Value("${spring.kafka.topic}")
     private String inputTopic;
@@ -45,66 +41,25 @@ public class Postnord {
     @Value("${spring.kafka.errortopic}")
     private String errorTopic;
 
-  
-
-    private final Tracer tracer =
-        GlobalOpenTelemetry.getTracer("io.opentelemetry.traces.hello",
-            tracesApiVersion);
-
-    private final Meter meter =
-        GlobalOpenTelemetry.meterBuilder("io.opentelemetry.metrics.hello")
-            .setInstrumentationVersion(metricsApiVersion)
-            .build();
-
-    private LongCounter numberOfExecutions;
-
     private static final Logger logger = LoggerFactory.getLogger(Postnord.class);
 
 
 
 
-    @PostConstruct
-    public void createMetrics() {
 
-        numberOfExecutions =
-            meter
-                .counterBuilder(Constants.NUMBER_OF_EXEC_NAME)
-                .setDescription(Constants.NUMBER_OF_EXEC_DESCRIPTION)
-                .setUnit("int")
-                .build();
-
-        meter
-            .gaugeBuilder(Constants.HEAP_MEMORY_NAME)
-            .setDescription(Constants.HEAP_MEMORY_DESCRIPTION)
-            .setUnit("byte")
-            .buildWithCallback(
-                r -> {
-                    r.record(getRuntime().totalMemory() - getRuntime().freeMemory());
-                });
-
-    }
     
     @PostMapping("/publishAsync")
     public void sendMessageAsync(@RequestBody User user){
-        Span span = tracer.spanBuilder("producer").startSpan();
-        try(Scope scope = span.makeCurrent()){
+        logger.debug("hellooooooo", errorTopic, user);
             producer.sendMessageAsync(inputTopic, errorTopic, user);
-            numberOfExecutions.add(1);
-        }finally {
-            span.end();
-        }
+       
        
     }
 
     @PostMapping("/publishsync")
     public void sendMessageSync(@RequestBody User user){
-        Span span = tracer.spanBuilder("producer").startSpan();
-        try(Scope scope = span.makeCurrent()){
             producer.sendMessageSync(inputTopic, errorTopic, user);
-            numberOfExecutions.add(1);
-        }finally {
-            span.end();
-        }
+        
        
     }
 }
